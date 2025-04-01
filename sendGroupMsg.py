bd1170ce6afe77727da4314133ee775742cfbfa1
linzhiji <linzhiji@ishehui.com>
2025-04-01 16:21:25 +0800

    fix bug

--- sendGroupMsg.py
+++ sendGroupMsg.py
@@ -10,23 +10,31 @@ load_dotenv()
 sdk_app_id = int(os.getenv('SDK_APP_ID', ''))
 secret_key = os.getenv('SECRET_KEY', '')
 admin_identifier = os.getenv('ADMIN_IDENTIFIER', '123456')
-
+fromAccount = "11002473"
 
 group_id = '73225880'  # 替换为你的群组ID
 message_text = 'ETH能到2000吗？'
 
-tim_client = TimClient(
-    app_id=sdk_app_id,
-    app_key=secret_key
-)
+# tim_client = TimClient(
+#     app_id=sdk_app_id,
+#     app_key=secret_key
+# )
 
-result = tim_client.group_open_http_svc.send_group_msg(
-    group_id=group_id,
-    msg_body=[{
-        "MsgType": "TIMTextElem",
-        "MsgContent": {
-            "Text": message_text
-        }
-    }])
+# result = tim_client.group_open_http_svc.send_group_msg(
+#     group_id=group_id,
+#     msg_body=[{
+#         "MsgType": "TIMTextElem",
+#         "MsgContent": {
+#             "Text": message_text
+#         }
+#     }])
 
-print(result)
+# print(result)
+
+from tencentcloud_im.tcim_client import TCIMClient, MessageText
+client = TCIMClient(sdk_app_id, secret_key, admin_identifier)
+
+msgTexgt = MessageText(message_text)
+client.send_group_message(group_id, 
+                          messageText=[message_text],
+                          from_account=fromAccount)
